32988e88a029010ef7e0fab17eb23e506069bd61
Andrew Ruiz <nukedoom@gmail.com>
2020-12-13 18:40:22 -0800

    Completed project

--- api/server.js
+++ api/server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const { OPEN_READWRITE } = require('sqlite3');
 const server = express();
+
+// Routers
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
+// Communicating in JSON
+server.use(express.json())
+
+// Making sure we got a signal
+server.get('/', (req, res) => {
+    res.send('Server is sending messages back.')
+})
+
+// Connecting Router
+
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
